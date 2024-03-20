8e597b0448384b6fcec42ef48a7439b201f193f0
ColdShaker <michaelball357@gmail.com>
2024-03-19 20:13:07 -0700

    fixed hamburger menu bug

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".menu-icon");
+    const icon = document.querySelector(".hamburger-icon");
+
+    /*The below code targets the .menu-links and .menu-icon class list,
+    and then adds another class called open. style.css has styles that only apply
+    to those classes when they are also accompanied by the open class.
+    The toggle keyword means that if the open class is already there, it will
+    instead remove it. */
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
